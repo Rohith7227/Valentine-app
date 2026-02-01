71680fa2a529a5dc07c3fd29748df42c83cd260c
Rohith7227 <165925376+Rohith7227@users.noreply.github.com>
2026-02-01 13:49:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+
+yesBtn.addEventListener("mouseenter", () => {
+  yesBtn.style.transform = "scale(3)";
+});
+
+yesBtn.addEventListener("mouseleave", () => {
+  yesBtn.style.transform = "scale(1)";
+});
+
+noBtn.addEventListener("mouseenter", () => {
+  const maxX = window.innerWidth - noBtn.offsetWidth;
+  const maxY = window.innerHeight - noBtn.offsetHeight;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  noBtn.style.position = "fixed";
+  noBtn.style.left = `${randomX}px`;
+  noBtn.style.top = `${randomY}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+  alert("Yayyy 💖 I knew it!");
+});
